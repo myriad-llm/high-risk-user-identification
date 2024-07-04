5a4aea03d9a719c156bfdd48da3bbb4658d39c97
Homing So <i@homing.so>
2024-07-04 13:08:41 +0800

    style: lint
    Signed-off-by: Homing So <i@homing.so>

--- models/lstm.py
+++ models/lstm.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class LSTM(nn.Module):
     def __init__(self, feature, hidden_size, num_layers, num_classes):
         super(LSTM, self).__init__()
@@ -8,7 +9,7 @@ class LSTM(nn.Module):
         self.num_layers = num_layers
         self.lstm = nn.LSTM(feature, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
-    
+
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
