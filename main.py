59aa23087ddab97dca1bdb8618b9a7aa5f8a53b1
Homing So <i@homing.so>
2024-07-05 17:35:50 +0800

    feat: update wandb logger
    Signed-off-by: Homing So <i@homing.so>

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from lightning.pytorch.cli import LightningCLI
+from lightning.pytorch.loggers import WandbLogger
 
 from models import LSTM, VAE, LSTM_VAE
 from datasets import CallRecordsDataModule
 
 
 def cli_main():
+    WandbLogger()
     cli = LightningCLI()
 
 
