12311d0524b7cc056e1d63ffb289521b79d96e5c
ShiWenber <1210169842@qq.com>
2024-07-06 11:26:47 +0800

    fix: wandb paths cannot be created before logs are generated

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from models import LSTM, VAE, LSTM_VAE
 from datasets import CallRecordsDataModule, CallRecords4VAEDataModule
 import os
 
-# 添加在代码执行之前为 wandb 新建目录的逻辑
 class MyLightningCLI(LightningCLI):
     def before_instantiate_classes(self) -> None:
         path = self.config_dump['trainer']['logger']['init_args']['save_dir']
@@ -12,7 +11,6 @@ class MyLightningCLI(LightningCLI):
         return super().before_instantiate_classes()
 
 def cli_main():
-    # print 这里获取的 config
     cli = MyLightningCLI()
 
 if __name__ == '__main__':
