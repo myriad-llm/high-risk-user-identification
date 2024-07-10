1547214cd93cba962b368b5742ff26796f3f57cc
ShiWenber <1210169842@qq.com>
2024-07-10 17:38:01 +0800

    feat: omegaconf and simplified configuration

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ class MyLightningCLI(LightningCLI):
     def before_instantiate_classes(self) -> None:
         path = self.config_dump["trainer"]["logger"]["init_args"]["save_dir"]
         os.makedirs(path, exist_ok=True)
-        return super().before_instantiate_classes()
 
 
 def cli_main():
-    cli = MyLightningCLI(save_config_kwargs={"overwrite": True})
+    cli = MyLightningCLI(save_config_kwargs={"overwrite": True}, parser_kwargs={"parser_mode": "omegaconf"})
 
 
 if __name__ == "__main__":
