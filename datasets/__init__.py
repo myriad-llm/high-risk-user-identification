4601ff8fd76ddaacf6773061306166c737fe8d4a
ShiWenber <1210169842@qq.com>
2024-07-10 22:45:43 +0800

    feat: callrecords for fata model

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,4 @@
 from .call_records import CallRecords
 from .call_records_dm import CallRecords4VAEDataModule, CallRecordsDataModule
 from .v2 import CallRecordsV2
+from .call_records_fata import CallRecordsFata
