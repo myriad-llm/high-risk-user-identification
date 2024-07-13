05fb95d279a5b8faf0daba54f40c7d7fe9dc2324
Homing So <i@homing.so>
2024-07-13 11:09:11 +0800

    feat: add BERT classification model

---
+++ models/bert_cls.py
@@ -0,0 +1,80 @@
+from typing import Any
+
+import lightning as L
+import torch
+from lightning.pytorch.utilities.types import OptimizerLRScheduler
+from torch import Tensor
+from torchmetrics.functional import accuracy
+from transformers import BertConfig
+from transformers import (
+    BertForSequenceClassification as HuBertForSequenceClassification,
+)
+from transformers.modeling_outputs import SequenceClassifierOutput
+
+
+class BertForSequenceClassification(L.LightningModule):
+    def __init__(
+        self,
+        input_size: int,
+        vocab_size: int,
+        hidden_size: int = 1152,
+        num_hidden_layers: int = 12,
+        num_classes: int = 2,
+    ) -> None:
+        super().__init__()
+
+        self.num_classes = num_classes
+        self.accuracy = accuracy
+        self.model = HuBertForSequenceClassification(
+            BertConfig(
+                vocab_size=vocab_size,
+                hidden_size=hidden_size,
+                num_hidden_layers=num_hidden_layers,
+                num_attention_heads=input_size,
+                num_labels=num_classes,
+            )
+        )
+
+    def training_step(self, batch, batch_idx) -> Tensor:
+        outputs: SequenceClassifierOutput = self.model(
+            input_ids=batch["input_ids"],
+            labels=batch["labels"],
+            return_dict=True,
+        )
+        self.log("train-loss", outputs.loss, batch_size=len(batch))
+        return outputs.loss
+
+    def validation_step(self, batch, batch_idx) -> None:
+        outputs: SequenceClassifierOutput = self.model(
+            input_ids=batch["input_ids"],
+            labels=batch["labels"],
+            return_dict=True,
+        )
+        self.log("val-loss", outputs.loss, batch_size=len(batch))
+
+        logits = outputs.logits
+        predicted_labels = torch.argmax(logits, 1)
+        acc = self.accuracy(
+            predicted_labels,
+            batch["label"],
+            num_classes=self.num_classes,
+            task="multiclass",
+        )
+        self.log("val-acc", acc, batch_size=len(batch), prog_bar=True)
+
+    def predict_step(self, batch, batch_idx):
+        outputs: SequenceClassifierOutput = self.model(
+            input_ids=batch["input_ids"],
+            labels=batch["labels"],
+            return_dict=True,
+        )
+
+        predicted_labels = torch.argmax(
+            torch.softmax(outputs.logits, dim=1),
+            dim=1,
+        )
+        return predicted_labels, None
+
+    def configure_optimizers(self) -> OptimizerLRScheduler:
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
