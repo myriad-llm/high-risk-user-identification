46d3ed3b3a567ae138bf69e7abbf307613b27f7a
Homing So <i@homing.so>
2024-07-13 12:21:35 +0800

    fix: update input_size because some model can not use input_size as zero
    Signed-off-by: Homing So <i@homing.so>

--- main.py
+++ main.py
@@ -13,18 +13,27 @@ class MyLightningCLI(LightningCLI):
         return super().before_instantiate_classes()
 
     def instantiate_classes(self) -> None:
-        self.config[self.subcommand].model.init_args.input_size = 0 # just need to add a arbitrary int value here
+        self.config[self.subcommand].model.init_args.input_size = (
+            1  # just need to add a arbitrary int value here
+        )
         config_init = self.parser.instantiate_classes(self.config)
-        fake_datamodule = config_init.get(str(self.subcommand), config_init).get('data')
-        if hasattr(fake_datamodule, 'feature_dim'):
-            self.config[self.subcommand].model.init_args.input_size = fake_datamodule.feature_dim
+        fake_datamodule = config_init.get(str(self.subcommand), config_init).get("data")
+        if hasattr(fake_datamodule, "feature_dim"):
+            self.config[self.subcommand].model.init_args.input_size = (
+                fake_datamodule.feature_dim
+            )
         else:
-            raise KeyError("Attribution 'feature_dim' not found in datamodule, you must implement it in datamodule")
+            raise KeyError(
+                "Attribution 'feature_dim' not found in datamodule, you must implement it in datamodule"
+            )
         return super().instantiate_classes()
 
 
 def cli_main():
-    cli = MyLightningCLI(save_config_kwargs={"overwrite": True}, parser_kwargs={"parser_mode": "omegaconf"})
+    cli = MyLightningCLI(
+        save_config_kwargs={"overwrite": True},
+        parser_kwargs={"parser_mode": "omegaconf"},
+    )
 
 
 if __name__ == "__main__":
